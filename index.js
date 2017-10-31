2039d311b68779c0cf5f8d69813878b7ab392e8c
Onur Eker <yonureker@gmail.com>
2017-10-31 03:55:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday,name) {
 }
 
 function holidayCountdown(holiday,days){
-  return (`Its ${days} days until ${holiday}!`)
+  return (`It's ${days} days until ${holiday}!`)
 }
