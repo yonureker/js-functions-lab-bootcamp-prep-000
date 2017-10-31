4ddee7e8094b3bd886c7b7411d84e1fe4e7a495c
Onur Eker <yonureker@gmail.com>
2017-10-31 03:53:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name){
-  return ("Happy Holidays, ${name}!")
+  return (`Happy Holidays, ${name}!`)
 }
 
 function happyHolidayTo(holiday,name) {
